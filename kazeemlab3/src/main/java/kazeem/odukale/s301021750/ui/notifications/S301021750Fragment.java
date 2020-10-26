937ef08df6af6ee6468651cced2565e63bccd8ed
kodukale <kodukale.my.centennialcollege.ca>
2020-10-26 14:25:00 -0400

    added mon and earth rotate functionality

--- kazeemlab3/src/main/java/kazeem/odukale/s301021750/ui/notifications/S301021750Fragment.java
+++ kazeemlab3/src/main/java/kazeem/odukale/s301021750/ui/notifications/S301021750Fragment.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,12 +21,35 @@ import kazeem.odukale.s301021750.R;
 public class S301021750Fragment extends Fragment {
 
     private S301021750ViewModel s301021750ViewModel;
+    private ImageView imageMoon;
+    private Animation an;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         s301021750ViewModel =
                 ViewModelProviders.of(this).get(S301021750ViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        final View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        Button start = root.findViewById(R.id.btnStartAnim);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageMoon = root.findViewById(R.id.moon);
+                // Load the appropriate animation
+                an =  AnimationUtils.loadAnimation(getContext(), R.anim.set);
+                // Start the animation
+                imageMoon.startAnimation(an);
+            }
+        });
+
+        Button stop = root.findViewById(R.id.btnStopAnim);
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageMoon.clearAnimation();
+            }
+        });
+
         return root;
     }
 }
--- kazeemlab3/src/main/java/kazeem/odukale/s301021750/ui/notifications/ViewWithBitmap.java
+++ kazeemlab3/src/main/java/kazeem/odukale/s301021750/ui/notifications/ViewWithBitmap.java
@@ -1,4 +1,41 @@
 package kazeem.odukale.s301021750.ui.notifications;
 
-public class ViewWithBitmap {
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.view.View;
+
+import kazeem.odukale.s301021750.R;
+
+public class ViewWithBitmap extends View {
+
+    // default constructor
+    public ViewWithBitmap(Context context) {
+        super(context);
+
+    }
+
+    @Override protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.BLUE);
+        Bitmap firstnameBitmap = BitmapFactory.decodeResource(getResources(), R.string.firstnmae);
+        Bitmap lastnameBitmap = BitmapFactory.decodeResource(getResources(), R.string.lastname);
+        Matrix topLeft = new Matrix();
+        topLeft.preRotate(45);
+        Matrix topRight = new Matrix();
+        topRight.preRotate(-45);
+
+        Bitmap firstnameTopLeft = Bitmap.createBitmap(firstnameBitmap, 0, 0, firstnameBitmap.getWidth(), firstnameBitmap.getHeight(), topLeft, false);
+        Bitmap lastnameTopRight = Bitmap.createBitmap(lastnameBitmap, 0,0, lastnameBitmap.getWidth(), lastnameBitmap.getHeight(), topRight, false);
+
+
+        // Free up some memory by dumping bitmaps we don't need anymore
+        firstnameBitmap.recycle();
+        lastnameBitmap.recycle();
+        //draw the bitmap in the canvas
+        canvas.drawBitmap(firstnameTopLeft, 30, 30, null);
+        canvas.drawBitmap(lastnameTopRight, 1025, 30, null);
+    }
 }
